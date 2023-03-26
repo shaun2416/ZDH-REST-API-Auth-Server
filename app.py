679fac381ef2742e67ee8510cb15de99d60ec78d
ZS\sc31079 <shaunak.chakraborty@zs.com>
2023-03-26 12:30:53 +0530

    removed run on port 5001

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def auth():
   })
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
   #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   #context.load_cert_chain('domain.crt', 'domain.key')
   #app.run(port = 5000, debug = True, ssl_context = context)
-  app.run(port = 5001, debug = True)
+  #app.run(port = 5001, debug = True)
